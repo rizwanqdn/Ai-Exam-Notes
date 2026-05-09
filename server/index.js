fa5d87a2710e1df75fcc7c56652d06b1a296b8ea
Rizwan Ahmad Zafar <137613067+rizwanqdn@users.noreply.github.com>
2026-05-09 14:10:52 +0530

    client url changed

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://ai-exam-notes-client-fik1.onrender.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   }),
